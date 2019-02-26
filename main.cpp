c464d85985a90a34851c4a24d037a8b1248bd35b
Shawn Halayka <sjhalayka@gmail.com>
2019-02-26 12:45:43 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -27,11 +27,19 @@ int main(void)
 		slices.push_back(slice);
 	}
 
-	size_t res = slices.size();
+	if (0 == slices.size())
+	{
+		cout << "No slices found" << endl;
+		return 2;
+	}
+
+	size_t x_res = slices[0].cols;
+	size_t y_res = slices[0].rows;
+	size_t z_res = slices.size();
 
 	vector<triangle> triangles;
-	vector<float> xyplane0(res*res, 0);
-	vector<float> xyplane1(res*res, 0);
+	vector<float> xyplane0(x_res*y_res, 0);
+	vector<float> xyplane1(x_res*y_res, 0);
 
 	size_t z = 0;
 	float threshold = 0.5f;
@@ -39,21 +47,21 @@ int main(void)
 	float grid_max = 1.0f;
 
 	// Calculate 0th xy plane.
-	for (size_t x = 0; x < res; x++)
-		for (size_t y = 0; y < res; y++)
-			xyplane0[x*res + y] = static_cast<float>(slices[z].at<unsigned char>(y, x) / 255.0f);
+	for (size_t x = 0; x < x_res; x++)
+		for (size_t y = 0; y < y_res; y++)
+			xyplane0[x*y_res + y] = static_cast<float>(slices[z].at<unsigned char>(y, x) / 255.0f);
 
 	// Prepare for 1st xy plane.
 	z++;
 
 	// Calculate 1st and subsequent xy planes.
-	for (; z < res; z++)
+	for (; z < z_res; z++)
 	{
-		cout << "Calculating triangles from xy-plane pair " << z << " of " << res - 1 << endl;
+		cout << "Calculating triangles from xy-plane pair " << z << " of " << z_res - 1 << endl;
 
-		for (size_t x = 0; x < res; x++)
-			for (size_t y = 0; y < res; y++)
-				xyplane1[x*res + y] = static_cast<float>(slices[z].at<unsigned char>(y, x) / 255.0f);
+		for (size_t x = 0; x < x_res; x++)
+			for (size_t y = 0; y < y_res; y++)
+				xyplane1[x*y_res + y] = static_cast<float>(slices[z].at<unsigned char>(y, x) / 255.0f);
 
 		// Calculate triangles for the xy-planes corresponding to z - 1 and z by marching cubes.
 		tesselate_adjacent_xy_plane_pair(
@@ -61,9 +69,9 @@ int main(void)
 			z - 1,
 			triangles,
 			threshold, // Use threshold as isovalue.
-			grid_min, grid_max, res,
-			grid_min, grid_max, res,
-			grid_min, grid_max, res);
+			grid_min, grid_max, x_res,
+			grid_min, grid_max, y_res,
+			grid_min, grid_max, z_res);
 
 		// Swap memory pointers (fast) instead of performing a memory copy (slow).
 		xyplane1.swap(xyplane0);
